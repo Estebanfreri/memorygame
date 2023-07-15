0f57c03a22e6d546ec1411dbc0800286c78d099a
Esteban Freri <estebanfreri@icloud.com>
2023-07-15 08:49:57 +0200

    first commit 2

--- assets/game.js
+++ assets/game.js
@@ -50,7 +50,7 @@ const generateGame = () => {
         throw new Error("The dimension of the board must be an even number.")
     }
 
-    const emojis = ['🥔', '🍒', '🥑', '🌽', '🥕', '🍇', '🍉', '🍌', '🥭', '☠️', '🦴', ' 🤡', '🍍', '🐞', ' 🍄', '🐌', '🕷️', '🧸', '🌼', '🐙', '👶', '👽', '🐝', '🪬']
+    const emojis = ['🍒', '🥑', '🥕', '🍇', '🍉', '🍌', '☠️', ' 🤡', '🐞', ' 🍄', '🕷️', '🧸', '🐙', '👽', '🪬']
     const picks = pickRandom(emojis, (dimensions * dimensions) / 2) 
     const items = shuffle([...picks, ...picks])
     const cards = `
